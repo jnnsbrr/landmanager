2334eb2081341a5b937a69497075a8850f285bf3
Stephen Wirth <wirth@pik-potsdam.de>
2025-03-31 18:38:25 +0200

    Add linter exceptions.

--- landmanager/components/management/crop_calendar/world.py
+++ landmanager/components/management/crop_calendar/world.py
@@ -1207,9 +1207,9 @@ def calc_doy_wet_month(daily_ppet):
     kernel = np.ones(window_size)
 
     # Perform convolution with periodic wrapping
-    extended_values = np.hstack([daily_values, daily_values[:, : window_size - 1]])
+    extended_values = np.hstack([daily_values, daily_values[:, : window_size - 1]])  # noqa
     running_sum = np.apply_along_axis(
-        lambda x: np.convolve(x, kernel, mode="valid"), axis=1, arr=extended_values
+        lambda x: np.convolve(x, kernel, mode="valid"), axis=1, arr=extended_values  # noqa
     )
 
     # Trim the running sum to match the original data length (365 days)
